2ad0d80e3e27aecb49ab9b76799081d26a5c46b4
guangdong <90661890@qq.com>
2018-01-26 18:56:49 +0800

    点餐台选择菜品数量，提交订单

--- order/urls.py
+++ order/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^desk=(?P<id>\d+)$', views.index),
     url(r'^orderaciton', views.order_aciton, name="orderaciton"),
+    url(r'^placeorder/$', views.place_order, name='place_order')
 ]
--- order/views.py
+++ order/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from checkstand.models import Menu, Kind, Desk
 from checkstand.models import Order, OrderDetail
+from django.http import JsonResponse
+import json
 
 
 # Create your views here.
@@ -8,7 +10,7 @@ def index(request, id):
     desk_id = Desk.objects.get(id=id, state=True).id
     kinds = Kind.objects.filter(state=True)
     menus = Menu.objects.filter(state=True)
-    return render(request, 'order/index.html', locals())
+    return render(request, 'order/orderpage.html', locals())
 
 
 def order_aciton(request):
@@ -22,3 +24,22 @@ def order_aciton(request):
         for checkbox in checkbox_list:
             OrderDetail.objects.create(num=1, menu_id=int(checkbox), order_id=int(order_id))
     return render(request, 'order/success.html')
+
+
+def place_order(request):
+    if request.method == 'POST':
+        desk_id = request.POST.get('desk_id')
+        try:
+            Order.objects.get(desk_id=desk_id, state=False)
+            return JsonResponse({'state': 'ordered'})
+        except Order.DoesNotExist:
+            order_menus = json.loads(request.POST.get('order_menu'))
+            remark = request.POST.get('remark')
+            totleprice = 0
+            for menu_id in order_menus:
+                totleprice += Menu.objects.get(id=menu_id).price * order_menus[menu_id]
+            order_id = Order.objects.create(desk_id=int(desk_id), remark=remark, totle_price=totleprice, state=0).id
+            for order_menu_id in order_menus:
+                OrderDetail.objects.create(num=int(order_menus[order_menu_id]), menu_id=int(order_menu_id),
+                                           order_id=int(order_id))
+            return JsonResponse({'state': 'success'})
